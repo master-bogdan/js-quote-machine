eb1fea467933f88e430ad25dea6f189679e3ef3a
masterbogdan <maiskiychavk@gmail.com>
2020-07-13 21:55:27 +0300

    integrated social share

--- app.js
+++ app.js
@@ -23,10 +23,26 @@ window.addEventListener('DOMContentLoaded', () => {
                     <div class="card-body">
                         <p class="card-text">${data.quoteText}</p>
                         <h6 id="author" class="card-subtitle mb-2 text-muted">${data.quoteAuthor}</h6>
-                        <a href="#" class="card-link">Card link</a>
-                        <a class="twitter-share-button"
-                            href="https://twitter.com/intent/tweet?text=${data.quoteText}">
-                        Tweet</a>
+                        <div class="social-container">
+                            <div class="fb-share-button btn-primary" 
+                                data-href="https://master-bogdan.github.io/js-quote-machine/" 
+                                data-layout="button" 
+                                data-size="large">
+                                    <a target="_blank" 
+                                    href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fmaster-bogdan.github.io%2Fjs-quote-machine%2F&amp;src=sdkpreparse&amp;quote=${data.quoteText} (c) ${data.quoteAuthor}" class="fb-xfbml-parse-ignore">
+                                    <i class="fab fa-facebook"></i> Поделиться
+                                    </a>
+                            </div>
+                            <div class="twitter-container">
+                                <a class="twitter-share-button btn-primary"
+                                    href="https://twitter.com/intent/tweet?hashtags=quotes,цитаты&related=masterbogdan&text=${data.quoteText} (c) ${data.quoteAuthor}"
+                                    target="_blank"
+                                    >
+                                    <i class="fab fa-twitter"></i>
+                                    Твитнуть
+                                </a>
+                            </div>
+                        </div>
                     </div>
                 </div>
             `;
@@ -42,10 +58,15 @@ window.addEventListener('DOMContentLoaded', () => {
         if (language === 'ru') {
             heading.textContent = 'Сайт случайной цитаты';
             btnGet.textContent = 'Получить случайную цитату';
+            // fbBtn.innerHTML = 'Поделится';
+            // twBtn.innerHTML = 'Твитнуть';
+            
         } 
         else { 
             heading.textContent = 'Random quote site';
-            btnGet.textContent = 'Get quote';
+            btnGet.textContent = 'Get random quote';
+            // fbBtn.innerHTML = 'Share';
+            // twBtn.innerHTML = 'Tweet';
         }
     }
 
